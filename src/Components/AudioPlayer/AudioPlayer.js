d1d624656c3f1ff9df0f2d994021684bc2f70660
Petros <psdeme@hotmail.com>
2020-06-19 17:58:55 +0300

    added audio play functionality on button play

--- src/Components/AudioPlayer/AudioPlayer.js
+++ src/Components/AudioPlayer/AudioPlayer.js
@@ -1,14 +1,29 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import SongDetails from './SongDetails';
 import { Store } from '../../Context/Store'
 
 function AudioPlayer() {
   const {state, dispatch} =  useContext(Store);
-  const {song} = state;
+  const {playbackStatus, song} = state;
+
+  useEffect(() => {
+    const audio = document.getElementById("audio");
+    if(playbackStatus === 'playing'){
+      audio.play();
+    } else {
+      audio.pause();
+    }
+  })
+
 
   return(
-  <SongDetails name={song ? song.name: ""} artist={song ? song.artist: ""}/>
-  )
+    <div className="player">
+    <audio id="audio" src={song && song.file}>
+    Your browser does not support the <code>audio</code> element.
+    </audio>
+    <SongDetails name={song ? song.name: ""} artist={song ? song.artist: ""}/>
+    </div>
+    )
 }
 
 export default AudioPlayer;
